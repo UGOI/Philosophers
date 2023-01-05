3c93a31c9e3d67d6affcbe6f7a1c561e30f422c8
Stefan Dukic <sdukic@3-B-10.42heilbronn.de>
2023-01-05 19:17:23 +0100

    fix some leaks

--- philo/src/main.c
+++ philo/src/main.c
@@ -6,12 +6,30 @@
 /*   By: sdukic <sdukic@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/04 19:08:12 by sdukic            #+#    #+#             */
-/*   Updated: 2023/01/04 19:08:16 by sdukic           ###   ########.fr       */
+/*   Updated: 2023/01/05 18:43:05 by sdukic           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include<stdio.h>
 #include"./include/philo.h"
+#include<stdlib.h>
+
+void	free_all(t_table *table, t_vars *vars)
+{
+	int	i;
+
+	i = 0;
+	while (i < vars->rules.num_of_philo)
+	{
+		pthread_mutex_destroy(&table->forks[i]);
+		i++;
+	}
+	pthread_mutex_destroy(vars->meal_check);
+	free(vars->meal_check);
+	free(table->forks);
+	free(table->philos);
+	return ;
+}
 
 int	main(int argc, char **argv)
 {
@@ -29,5 +47,6 @@ time_to_die time_to_eat time_to_sleep \
 		return (1);
 	if (create_threads(&table, &vars) == 0)
 		return (1);
+	free_all(&table, &vars);
 	return (0);
 }
